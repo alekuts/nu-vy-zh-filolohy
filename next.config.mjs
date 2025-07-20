b09ed5f37e2c385fefd6ce2eaf629aea0307b4eb
AleKutS <alexanderkutsovol@gmail.com>
2025-07-20 20:34:53 +0300

    Build to docs folder

--- next.config.mjs
+++ next.config.mjs
@@ -8,5 +8,6 @@ const withNextra = nextra({
 // Export the final Next.js config with Nextra included
 export default withNextra({
   // ... Add regular Next.js options here
-  output: 'export'
+  output: 'export',
+  distDir: 'docs',
 })
