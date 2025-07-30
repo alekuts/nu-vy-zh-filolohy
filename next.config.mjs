81dd6a3a57721eb4fd01829d1ff7fb10fe2d8f62
AleKutS <alexanderkutsovol@gmail.com>
2025-07-30 12:33:04 +0300

    upd

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,8 @@
 import nextra from 'nextra'
  
 // Set up Nextra with its configuration
-const withNextra = nextra({
-  theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.js', // вказуємо шлях до конфігу
-  // ... Add Nextra-specific options here
-})
- 
+ const withNextra = nextra('nextra-theme-docs') // або 'nextra-theme-blog'
+
 // Export the final Next.js config with Nextra included
 export default withNextra({
   // ... Add regular Next.js options here
