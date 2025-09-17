7770e2d8d30df20bf15868f766d27f82a389ae57
AleKutS <alexanderkutsovol@gmail.com>
2025-09-17 15:19:10 +0300

    upd

--- app/layout.jsx
+++ app/layout.jsx
@@ -56,7 +56,7 @@ export default async function RootLayout({ children }) {
       <body>
         <Layout
           search={false}
-          banner={banner}
+          //banner={banner}
           navbar={navbar}
           pageMap={await getPageMap()}
           //footer={footer}
